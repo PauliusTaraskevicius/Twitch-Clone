93df26d9e2eeb432809e4d0490eea9e839080303
PauliusTaraskevicius <fixas1029@gmail.com>
2024-02-07 17:26:48 +0200

    follow list

--- app/(browse)/_components/sidebar/recommended.tsx
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -19,16 +19,25 @@ export const Recommended = ({ data }: RecommendedProps) => {
   const showLabel = !collapsed && data.length > 0;
 
   return (
-    <ul className="space-y-2 px-2">
-      {data.map((user) => (
-        <UserItem
-          key={user.id}
-          username={user.username}
-          imageUrl={user.imageUrl}
-          //   isLive={user.stream?.isLive}
-        />
-      ))}
-    </ul>
+    <div>
+      {showLabel && (
+        <div className="pl-6 mb-4">
+          <p className="text-sm text-muted-foreground">
+            Recommended
+          </p>
+        </div>
+      )}
+      <ul className="space-y-2 px-2">
+        {data.map((user) => (
+          <UserItem
+            key={user.id}
+            username={user.username}
+            imageUrl={user.imageUrl}
+            // isLive={user.stream?.isLive}
+          />
+        ))}
+      </ul>
+    </div>
   );
 };
 
