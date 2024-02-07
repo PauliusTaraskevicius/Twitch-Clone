46baeed80c24088dfa9854663fa1a5fa9a1dc369
PauliusTaraskevicius <fixas1029@gmail.com>
2024-02-07 17:08:02 +0200

    follow service

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
@@ -28,6 +29,7 @@ export default function RootLayout({
             forcedTheme="dark"
             storageKey="gamehub-theme"
           >
+            <Toaster theme="light" position="bottom-center" />
             {children}
           </ThemeProvider>
         </body>
