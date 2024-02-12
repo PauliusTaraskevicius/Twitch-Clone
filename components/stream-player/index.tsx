75e181ab6dbb3b000a47ce037ba4fcd967104b10
PauliusTaraskevicius <fixas1029@gmail.com>
2024-02-12 16:54:08 +0200

    video component

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -1,6 +1,11 @@
 "use client";
 
+import { Stream, User } from "@prisma/client";
+import { LiveKitRoom } from "@livekit/components-react";
+
+import { cn } from "@/lib/utils";
 import { useViewerToken } from "@/hooks/user-viewer-token";
+import Video from "./video";
 
 type CustomStream = {
   id: string;
@@ -38,5 +43,17 @@ export default function StreamPlayer({
     <div>Cannot watch the stream</div>;
   }
 
-  return <div>Allowed to watch the stream</div>;
+  return (
+    <>
+      <LiveKitRoom
+        token={token}
+        serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
+        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+      >
+        <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+          <Video hostName={user.username} hostIdentity={user.id} />
+        </div>
+      </LiveKitRoom>
+    </>
+  );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["utfs.io"],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
