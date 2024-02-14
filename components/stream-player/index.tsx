45b3e09a5a2884cdeddabc351cfcfab433044b2e
PauliusTaraskevicius <fixas1029@gmail.com>
2024-02-14 10:01:01 +0200

    chat component

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -5,7 +5,10 @@ import { LiveKitRoom } from "@livekit/components-react";
 
 import { cn } from "@/lib/utils";
 import { useViewerToken } from "@/hooks/user-viewer-token";
-import Video from "./video";
+import Video, { VideoSkeleton } from "./video";
+import { useChatSidebar } from "@/store/use-chat-sidebar";
+import Chat from "./chat";
+import ChatToggle from "./chat-toggle";
 
 type CustomStream = {
   id: string;
@@ -38,6 +41,7 @@ export default function StreamPlayer({
   isFollowing,
 }: StreamPlayerProps) {
   const { token, name, identity } = useViewerToken(user.id);
+  const { collapsed } = useChatSidebar((state) => state);
 
   if (!token || !name || !identity) {
     <div>Cannot watch the stream</div>;
@@ -45,15 +49,67 @@ export default function StreamPlayer({
 
   return (
     <>
+      {collapsed && (
+        <div className="hidden lg:block fixed top-[100px] right-2 z-50">
+          <ChatToggle />
+        </div>
+      )}
       <LiveKitRoom
         token={token}
         serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
-        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+        className={cn(
+          "grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
+          collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
+        )}
       >
         <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
           <Video hostName={user.username} hostIdentity={user.id} />
+          {/* <Header
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            imageUrl={user.imageUrl}
+            isFollowing={isFollowing}
+            name={stream.name}
+          /> */}
+          {/* <InfoCard
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            name={stream.name}
+            thumbnailUrl={stream.thumbnailUrl}
+          /> */}
+          {/* <AboutCard
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            bio={user.bio}
+            followedByCount={user._count.followedBy}
+          /> */}
+        </div>
+        <div className={cn("col-span-1", collapsed && "hidden")}>
+          <Chat
+            viewerName={name}
+            hostName={user.username}
+            hostIdentity={user.id}
+            isFollowing={isFollowing}
+            isChatEnabled={stream.isChatEnabled}
+            isChatDelayed={stream.isChatDelayed}
+            isChatFollowersOnly={stream.isChatFollowersOnly}
+          />
         </div>
       </LiveKitRoom>
     </>
   );
 }
+
+export const StreamPlayerSkeleton = () => {
+  return (
+    <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
+      <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+        <VideoSkeleton />
+        {/* <HeaderSkeleton /> */}
+      </div>
+      <div className="col-span-1 bg-background">{/* <ChatSkeleton /> */}</div>
+    </div>
+  );
+};
