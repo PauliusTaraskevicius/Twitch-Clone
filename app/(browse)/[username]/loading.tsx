3e564d885dc8c3f493ff9a95320a714343106169
PauliusTaraskevicius <fixas1029@gmail.com>
2024-02-15 11:12:27 +0200

    user page

---
+++ app/(browse)/[username]/loading.tsx
@@ -0,0 +1,9 @@
+import { StreamPlayerSkeleton } from "@/components/stream-player";
+
+export const UserLoading = () => {
+  return (
+    <div className="h-full">
+      <StreamPlayerSkeleton />
+    </div>
+  );
+};
--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -4,6 +4,7 @@ import { getUserByUsername } from "@/lib/user-service";
 import { isFollowingUser } from "@/lib/follow-service";
 import { Actions } from "./_components/actions";
 import { isBlockedByUser } from "@/lib/block-service";
+import { StreamPlayer } from "@/components/stream-player";
 
 interface UserPageProps {
   params: {
@@ -14,7 +15,7 @@ interface UserPageProps {
 const UserPage = async ({ params }: UserPageProps) => {
   const user = await getUserByUsername(params.username);
 
-  if (!user) {
+  if (!user || !user.stream) {
     notFound();
   }
 
@@ -25,11 +26,12 @@ const UserPage = async ({ params }: UserPageProps) => {
     notFound();
   }
 
+  if (isBlocked) {
+    notFound();
+  }
+
   return (
-    <div className="flex flex-col gap-y-4">
-      {user.username} is following: {`${isFollowing}`}
-      <Actions isFollowing={isFollowing} userId={user.id} />
-    </div>
+    <StreamPlayer user={user} stream={user.stream} isFollowing={isFollowing} />
   );
 };
 
